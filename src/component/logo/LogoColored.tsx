9ea06a6bc4c89705378531fc795c1bc4ac4d26ba
Olumide Michael <olumide.m.ojo@gmail.com>
2024-02-05 12:54:01 +0100

    feat: created second logo component

---
+++ src/component/logo/LogoColored.tsx
@@ -0,0 +1,24 @@
+import styles from "./Logo.module.scss";
+import logo from "/elev8trix-logo-colored.svg";
+
+type logoProps = {
+  link?: string;
+  style?: React.CSSProperties;
+};
+
+const LogoColored = ({ link, style }: logoProps) => {
+  return (
+    <div className={styles.logoContainer}>
+      <a href={link} className={styles.logoLink}>
+        <img
+          className={styles.logo}
+          alt="Elev8Trix Logo"
+          src={logo}
+          style={style}
+        />
+      </a>
+    </div>
+  );
+};
+
+export default LogoColored;
