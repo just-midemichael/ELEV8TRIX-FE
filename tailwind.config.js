dc23a8beeab24953bbfda18cdc448b6cbd6fc1b5
Olumide Michael <olumide.m.ojo@gmail.com>
2024-02-05 23:44:17 +0100

    chore: install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,102 @@
+/** @type {import('tailwindcss').Config} */
+
+// const defaultTheme = require("tailwindcss/defaultTheme");
+
+// module.exports = {
+//   content: [
+//     // Or if using `src` directory:
+//     "./src/**/*.{js,ts,jsx,tsx}"
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary50: "#FFF9F6",
+//         primary100: "#FFF3EA",
+//         primary200: "#93b8fa",
+//         primary300: "#6297f8",
+//         primary400: "#FE8501",
+//         primary500: "#1464f4",
+//         primary600: "#125bde",
+//         primary700: "#0e47ad",
+//         primary800: "#0b3786",
+//         primary900: "#082a66",
+
+//         grey20: "#F0F5F7",
+//         grey30: "#DDDDDD",
+//         grey40: "#717171",
+//         grey50: "#252525",
+//         grey60: "#696969",
+//         grey70: "#424242",
+//         grey80: "#B3B3B3",
+//         grey90: "#545454",
+//         grey100: "#DFDFE6",
+
+//         black10: "#D7D7D7",
+//         black20: "#070707",
+//         black: "#2A2A2B",
+
+//         white10: "#FCFCFD",
+
+//         green10: "#05A660",
+//         green20: "#EBF8F1",
+//         red10: "#E53535",
+//         red20: "#FFEBEB",
+//         blue10: "#3772FF",
+//         yellow10: "#FF8800",
+//         yellow20: "#FFF3D8",
+
+//         orange50: "#FFF3E6",
+//         orange100: "#FED9B0",
+//         orange200: "#FEC78A",
+//         orange300: "#FEAD55",
+//         orange400: "#FD9D34",
+//         orange500: "#FD8501",
+//         orange600: "#E60F01",
+//         orange700: "#B45E01",
+//         orange800: "#8B0901",
+//         orange900: "#6A3800",
+
+//         danger50: "#FFE7E6",
+//         danger100: "#FEB5B0",
+//         danger200: "#FE918A",
+//         danger300: "#FE5F55",
+//         danger400: "#FE5F55",
+//         danger500: "#FD1001",
+//         danger600: "#E60F01",
+//         danger700: "#B40B01",
+//         danger800: "#8B0901",
+//         danger900: "#6A0700",
+
+//         warning50: "#FEF6E6",
+//         warning100: "#FDE3B2",
+//         warning200: "#FCD68D",
+//         warning300: "#FAC359",
+//         warning400: "#F9B839",
+//         warning500: "#F8A607",
+//         warning600: "#E29706",
+//         warning700: "#B07605",
+//         warning800: "#885B04",
+//         warning900: "#684603",
+
+//         success50: "#E7F8EF",
+//         success100: "#B4EACD",
+//         success200: "#8FE0B5",
+//         success300: "#5CD193",
+//         success400: "#3DC97E",
+//         success500: "#0CBB5E",
+//         success600: "#0BAA56",
+//         success700: "#098543",
+//         success800: "#076734",
+//         success900: "#054F27"
+//       },
+//       fontFamily: {
+//         playfair: ['"Playfair Display"', "system-ui"],
+//         poppins: ['"Poppins"', "system-ui"],
+//         inter: ['"inter"', "system-ui"],
+//         roboto: ['"roboto"', "system-ui"],
+//         cal: ['"OpenSans"', "system-ui"],
+//         matter: ['"Matter"', "system-ui"]
+//       }
+//     }
+//   }
+// };
