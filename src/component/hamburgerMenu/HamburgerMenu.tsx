c849674c9e78fb8517eb8c1ba01b941f8c7fd9ee
Olumide Michael <olumide.m.ojo@gmail.com>
2024-02-06 07:17:47 +0100

    style: fix style error

--- src/component/hamburgerMenu/HamburgerMenu.tsx
+++ src/component/hamburgerMenu/HamburgerMenu.tsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export const HamburgerMenu = () => {
   const activeStyle = ({ isActive }: { isActive: boolean }) => {
     return {
-      color: isActive ? "red" : ""
+      color: isActive ? "black" : ""
     };
   };
 
