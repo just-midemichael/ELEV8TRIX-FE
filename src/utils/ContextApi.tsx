3352c908da2ab3dbdea8608fb657f14415c0031e
Olumide Michael <olumide.m.ojo@gmail.com>
2024-02-25 21:19:31 +0100

    feat: create context API

---
+++ src/utils/ContextApi.tsx
@@ -0,0 +1,22 @@
+import React, { createContext, useContext, useState } from "react";
+import { LinkContextProps } from "./type";
+
+// Create Context
+const globalContext = createContext<LinkContextProps>({} as LinkContextProps);
+
+export const ContextApi = ({ children }: { children: React.ReactNode }) => {
+  //Set Course Link
+  const [link, setLink] = useState("/courses");
+
+  return (
+    <globalContext.Provider value={{ link, setLink }}>
+      {children}
+    </globalContext.Provider>
+  );
+};
+
+// Allow us to use the context created
+// eslint-disable-next-line react-refresh/only-export-components
+export const useGlobalContex = () => {
+  return useContext(globalContext);
+};
---
+++ src/utils/type.ts
@@ -0,0 +1,4 @@
+export type LinkContextProps = {
+  link: string;
+  setLink: React.Dispatch<React.SetStateAction<string>>;
+};
