f77449878420e7cf5eaaaa47a0168a0e12ce7ff4
Olumide Michael <olumide.m.ojo@gmail.com>
2024-02-27 10:52:23 +0100

    feat: add more data

--- src/component/testimony/Testimonials.ts
+++ src/component/testimony/Testimonials.ts
@@ -1,11 +1,19 @@
 import avatar1 from "/avatar1.png";
 import avatar2 from "/avatar2.png";
+import avatar3 from "/avatar3.png";
+import avatar4 from "/avatar4.png";
 
 export const content = [
   "Content 1 Lorem ipsum dolor sit amet consectetur. Ut porttitor aliquam laoreet enim. Sapien faucibus nam sagittis tempus suspendisse enim at. Elit aliquam id malesuada feugiat gravida elementum. Facilisis viverra interdum platea lectus. Sit arcu egestas laoreet scelerisque integer. Amet diam libero sit rutrum vitae felis. Vitae mattis interdum justo tortor malesuada. Turpis cursus placerat condimentum consectetur lectus feugiat suspendisse volutpat donec",
   "Content 2 Lorem ipsum dolor sit amet consectetur. Ut porttitor aliquam laoreet enim. Sapien faucibus nam sagittis tempus suspendisse enim at. Elit aliquam id malesuada feugiat gravida elementum. Facilisis viverra interdum platea lectus. Sit arcu egestas laoreet scelerisque integer. Amet diam libero sit rutrum vitae felis. Vitae mattis interdum justo tortor malesuada. Turpis cursus placerat condimentum consectetur lectus feugiat suspendisse volutpat donec",
-  "Content 3 Lorem ipsum dolor sit amet consectetur. Ut porttitor aliquam laoreet enim. Sapien faucibus nam sagittis tempus suspendisse enim at. Elit aliquam id malesuada feugiat gravida elementum. Facilisis viverra interdum platea lectus. Sit arcu egestas laoreet scelerisque integer. Amet diam libero sit rutrum vitae felis. Vitae mattis interdum justo tortor malesuada. Turpis cursus placerat condimentum consectetur lectus feugiat suspendisse volutpat donec"
+  "Content 3 Lorem ipsum dolor sit amet consectetur. Ut porttitor aliquam laoreet enim. Sapien faucibus nam sagittis tempus suspendisse enim at. Elit aliquam id malesuada feugiat gravida elementum. Facilisis viverra interdum platea lectus. Sit arcu egestas laoreet scelerisque integer. Amet diam libero sit rutrum vitae felis. Vitae mattis interdum justo tortor malesuada. Turpis cursus placerat condimentum consectetur lectus feugiat suspendisse volutpat donec",
+  "Content 4 Lorem ipsum dolor sit amet consectetur. Ut porttitor aliquam laoreet enim. Sapien faucibus nam sagittis tempus suspendisse enim at. Elit aliquam id malesuada feugiat gravida elementum. Facilisis viverra interdum platea lectus. Sit arcu egestas laoreet scelerisque integer. Amet diam libero sit rutrum vitae felis. Vitae mattis interdum justo tortor malesuada. Turpis cursus placerat condimentum consectetur lectus feugiat suspendisse volutpat donec"
 ];
-export const avatar = [`${avatar1}`, `${avatar2}`, `${avatar1}`];
-export const name = ["Name 1", "Name 2", "Name 3"];
-export const occupation = ["Occupation 1", "Occupation 2", "Occupation 3"];
+export const avatar = [`${avatar1}`, `${avatar2}`, `${avatar3}`, `${avatar4}`];
+export const name = ["Name 1", "Name 2", "Name 3", "Name 4"];
+export const occupation = [
+  "Occupation 1",
+  "Occupation 2",
+  "Occupation 3",
+  "Occupation 3"
+];
